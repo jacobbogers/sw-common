6b97bc092b80b73828f24e47b022a4f97525dff8
Jacob Bogers <jkfbogers@gmail.com>
2025-06-05 22:42:24 +0200

    making great traction

--- packages/protonmail/src/routes/+layout.svelte
+++ packages/protonmail/src/routes/+layout.svelte
@@ -70,6 +70,16 @@
     hreflang="pt-PT"
     href="https://account.proton.me/pt/mail"
   />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="anonymous"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    rel="stylesheet"
+  />
   <!--
   <link
     rel="manifest"
