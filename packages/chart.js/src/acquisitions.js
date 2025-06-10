b92959d2f5747affd552a4106b1fdefb663bfcbd
jacobbogers <jkfbogers@gmail.com>
2025-06-10 14:17:15 +0200

    chartjs turtle

--- packages/chart.js/src/acquisitions.js
+++ packages/chart.js/src/acquisitions.js
@@ -1,22 +1,46 @@
 // @ts-nocheck
-import Chart from 'chart.js/auto'
+import {
+  Chart,
+  Colors,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Legend
+} from 'chart.js'
+
+Chart.register(
+  Colors,
+  BarController,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Legend
+);
+
+import { getAquisitionsByYear } from './api'
 
 (async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  const data = await getAquisitionsByYear();
 
-  const canvasNode = document.querySelector('#acquisitions');
+  // nonbody say you need to use an id aslong as it is 
+  const canvasNodeWrapper = document.querySelector('[data-chart-name="i-have-canvas"]');
+  const canvasNode = canvasNodeWrapper.querySelector('canvas');
   new Chart(
     canvasNode,
     {
       type: 'bar',
+      options: {
+        animation: false,
+        plugins: {
+          legend: {
+            display: true
+          },
+          tooltip: {
+            enabled: true
+          }
+        }
+      },
       data: {
         labels: data.map(row => row.year),
         datasets: [
