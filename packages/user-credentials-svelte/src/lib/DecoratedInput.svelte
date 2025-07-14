fc1caddf4f4483a95f7f130114a082189b1b5828
jacobbogers <jkfbogers@gmail.com>
2025-07-14 11:37:15 +0200

    new svelte scaffold

--- packages/user-credentials-svelte/src/lib/DecoratedInput.svelte
+++
@@ -1,7 +0,0 @@
-<input />
-<i class="fa-solid fa-eye" />
-<i class="fa-solid fa-eye-slash"></i>
-<i class="fa-solid fa-key" />
-<i class="fa-solid fa-circle-xmark"></i>
-<style>
-</style>
--- packages/user-credentials-svelte/src/routes/_layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import '../app.css';
-</script>
-
-<svelte:head>
-	<title>Decorated Input Control</title>
-</svelte:head>
-
-<slot></slot>
