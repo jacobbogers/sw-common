9f8d6d3d2ff5e14353fe9826fb1896dd423a11ad
jacobbogers <jkfbogers@gmail.com>
2025-07-14 13:54:43 +0200

    more scaffolding

--- packages/user-credentials-svelte/src/routes/+layout.svelte
+++ packages/user-credentials-svelte/src/routes/+layout.svelte
@@ -1,40 +1,55 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import '../app.css';
-
 	let { children } = $props();
 </script>
 
 <div class="app">
-	<Header />
-
+	<navbar>
+		this is a navbar
+	</navbar>
+	<siderbar>
+		this is a sidebar
+	</siderbar>		
 	<main>
 		{@render children()}
 	</main>
 
 	<footer>
 		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+			this is a footer
 		</p>
 	</footer>
 </div>
 
 <style>
 	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
+		height: 100%;
+		display: grid;
+		grid-template: 
+		   "navbar navbar" auto
+		   "siderbar content"  1fr
+		   "footer footer" auto
+		   / auto 1fr
 	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
+	:global(footer) {
+		grid-area: footer
+	}
+
+	:global(main) {
+		grid-area: content;
+		place-self: stretch;
+		height: 100%;
+		border: 4px solid blue;
+		
+	}
+
+	:global(navbar) {
+		grid-area: navbar;
+	}
+
+	:global(siderbar) {
+		grid-area: siderbar;
 	}
 
 	footer {
@@ -42,16 +57,12 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		padding: 12px;
+		padding: 12px
 	}
-
-	footer a {
-		font-weight: bold;
-	}
-
+	
 	@media (min-width: 480px) {
 		footer {
-			padding: 12px 0;
+			padding: 12px 0
 		}
 	}
 </style>
